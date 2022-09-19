650e57817f295e13d17e8d43b920acb3826c1d5e
Leonardo Chuello <ljchuello@gmail.com>
2022-09-19 18:13:29 -0500

    .

---
+++ src/app/graficas/graficas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraficasRoutingModule } from './graficas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GraficasRoutingModule
+  ]
+})
+export class GraficasModule { }
