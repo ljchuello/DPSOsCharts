2a7408a72c55d2419061621d2d60f6e76f9f04fa
Leonardo Chuello <ljchuello@gmail.com>
2022-09-19 18:46:51 -0500

    Modificando cosas!.

--- src/app/graficas/graficas-routing.module.ts
+++ src/app/graficas/graficas-routing.module.ts
@@ -1,10 +1,43 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MenuComponent} from '../shared/components/menu/menu.component';
+import {BarrasComponent} from './pages/barras/barras.component';
+import {BarrasDoblesComponent} from './pages/barras-dobles/barras-dobles.component';
+import {DonaComponent} from './pages/dona/dona.component';
+import {DonaHttpComponent} from './pages/dona-http/dona-http.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MenuComponent,
+    children: [
+      {
+        path: 'barra',
+        component: BarrasComponent,
+      },
+      {
+        path: 'barra-doble',
+        component: BarrasDoblesComponent,
+      },
+      {
+        path: 'dona',
+        component: DonaComponent,
+      },
+      {
+        path: 'donaHttp',
+        component: DonaHttpComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'barra',
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class GraficasRoutingModule { }
+export class GraficasRoutingModule {
+}
